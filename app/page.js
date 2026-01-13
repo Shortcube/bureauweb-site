2626fc71f6101c2710f36e84aa285a5ab23b975d
Shortcube <40771638+Shortcube@users.noreply.github.com>
2026-01-13 16:06:56 -0500

    Convert page.js to a Server Component
    Removed 'use client' directive to convert page to a Server Component. Updated comments to reflect component interactivity.

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
-'use client'
+// SUPPRIMÉ 'use client' - Cette page est maintenant un Server Component
+// Seuls les composants avec interactivité ont 'use client'
 
 import { Header, Footer } from '@/components/layout'
 import { 
@@ -17,40 +18,40 @@ import { LeadForm } from '@/components/forms'
 export default function Home() {
   return (
     <main className="min-h-screen">
-      {/* Navigation */}
+      {/* Navigation - 'use client' dans Header.js */}
       <Header />
       
-      {/* Hero Section */}
+      {/* Hero Section - 'use client' dans Hero.js */}
       <Hero />
       
-      {/* Trust Bar */}
+      {/* Trust Bar - Server Component */}
       <TrustBar />
       
-      {/* Problem Section */}
+      {/* Problem Section - Server Component */}
       <Problem />
       
-      {/* Solution Section */}
+      {/* Solution Section - Server Component */}
       <Solution />
       
-      {/* What's NOT Included */}
+      {/* What's NOT Included - Server Component */}
       <NotIncluded />
       
-      {/* Process / How it Works */}
+      {/* Process / How it Works - Server Component */}
       <Process />
       
-      {/* Pricing */}
+      {/* Pricing - 'use client' dans Pricing.js */}
       <Pricing />
       
-      {/* Google Reviews System */}
+      {/* Google Reviews System - Server Component */}
       <Reviews />
       
-      {/* Compliance / Loi 25 */}
+      {/* Compliance / Loi 25 - Server Component */}
       <Compliance />
       
-      {/* Lead Capture Form */}
+      {/* Lead Capture Form - 'use client' dans LeadForm.js */}
       <LeadForm />
       
-      {/* Footer */}
+      {/* Footer - Server Component */}
       <Footer />
     </main>
   )
